56e129bd60f43811317b11be0650c8e7efccfbf2
namnameeroo <nami.skageec@gmail.com>
2022-10-09 18:32:34 +0900

    files: making main page template

--- src/diabetes-cra/src/App.js
+++ src/diabetes-cra/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Link} from "react-router-dom";
+// import styled from "styled-component";
 import LoginPage from "./pages/login";
 import MainPage from "./pages/main";
 
@@ -12,6 +13,14 @@ function App() {
 
   return (
     <div style={container}>
+      <div className="temp">
+        <li>
+          <Link to="/login">로그인페이지로</Link>
+        </li>
+        <li>
+          <Link to="/main">메인페이지로</Link>
+        </li>
+      </div>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/main" element={<MainPage />} />
--- src/diabetes-cra/src/pages/main.jsx
+++ src/diabetes-cra/src/pages/main.jsx
@@ -1,3 +1,24 @@
-export default function MainPage() {
-  return <div>"hi main page"</div>;
+import React from "react";
+import "../styles/main.css";
+import Footer from "../components/footer";
+
+const PageTitle = (props) => {
+  return (
+    <h3 className="page_title">
+      <div className="page_title_inner">{props.children}</div>
+    </h3>
+  );
+};
+const MainContainer = () => {};
+
+function MainPage() {
+  return (
+    <div id="wrap" className="wrap">
+      <PageTitle>음식 입력하기</PageTitle>
+      <MainContainer />
+      <Footer />
+    </div>
+  );
 }
+
+export default MainPage;
