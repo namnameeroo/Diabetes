038c6ae8758e25d4b6817952ac45546c30dea832
HanaHww2 <hanahww2@gmail.com>
2022-10-28 17:19:47 +0900

    refactor: 식품 관련 기능 로직 수정 및 보완 #27

--- back_end/src/main/java/com/diabetes/food/FoodRepository.java
+++ back_end/src/main/java/com/diabetes/food/FoodRepository.java
@@ -5,8 +5,11 @@ import com.diabetes.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
     List<Food> findAllByUser(User user);
     List<Food> findAllByUserId(Long userId);
+
+    Optional<Food> findByIdAndUserId(Long foodId, Long userId);
 }
--- back_end/src/main/java/com/diabetes/food/dto/FoodDto.java
+++ back_end/src/main/java/com/diabetes/food/dto/FoodDto.java
@@ -4,23 +4,42 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Builder
 @AllArgsConstructor
 public class FoodDto {
 
     private Long id;
+    @NotNull
     private Long userId;
+
+    @NotNull
     private String name;
+    @NotNull
     private String provider;
-    private Integer entireWeight;
-    private Integer calories;
-    private Integer carbohydrate;
-    private Integer protein;
-    private Integer fat;
-    private Integer intake;
-    private Integer remains;
-    private Integer gl;
+    @NotNull
+    private Float entireWeight;
+    @NotNull
+    private Float calories;
+    @NotNull
+    private Float carbohydrate;
+    @NotNull
+    private Float protein;
+    @NotNull
+    private Float fat;
+    @NotNull
+    private Float intake;
+    @NotNull
+    private Float gl;
+    @NotNull
     private String result;
 
+    public void checkUserId(Long userId) {
+        if (!this.userId.equals(userId)) {
+            // TODO 에러를 던질 것인지?
+            this.userId = userId;
+        }
+    }
 }
