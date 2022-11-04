a6c19d854fa0f47d745a2bdb9a6f1dede5a547e9
namnameeroo <nami.skageec@gmail.com>
2022-11-04 16:42:06 +0900

    refactor:fix:11:linkFixByRename #11

--- front_end/src/pages/foodForm.jsx
+++ front_end/src/pages/foodForm.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "../styles/main.css";
 import Footer from "../components/footer";
 
@@ -22,9 +21,12 @@ const InputCell = (props) => {
 };
 
 const handleFormSubmit = (e) => {
+  // const [errorMsg, setErrorMsg] = React.useState("에러 발생");
+
   console.log(e.target, "값");
   console.log(e.target[0]);
   console.log(e);
+
   console.log("form 전송");
   // setCounter(counter + 1);
   e.preventDefault();
@@ -49,6 +51,78 @@ const Today = () => {
 
   return <div className="right-align small-txt gray-txt">작성일 : {[todayYear, todayMonth, todayDay].join("-")}</div>;
 };
+const InputForm = () => {
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <div className="main_wrap table_wrap">
+        <table className="simple_font">
+          <tbody>
+            <tr>
+              <td className="pad-right-10">제품명</td>
+              <td>
+                <InputCell />
+              </td>
+            </tr>
+            <tr>
+              <td className="pad-right-10">업체명</td>
+              <td>
+                <InputCell />
+              </td>
+            </tr>
+            <tr>
+              <td className="pad-right-10">총량&nbsp; &nbsp; &nbsp; &nbsp;(g)</td>
+              <td>
+                <InputCell />
+              </td>
+            </tr>
+            {/* 단위들 스팬에 넣고... 공백 맞추기!*/}
+            <tr>
+              <td className="pad-right-10">섭취량&nbsp; &nbsp; &nbsp;(%)</td>
+              <td>
+                <InputCell />
+              </td>
+            </tr>
+            <tr>
+              <td className="pad-right-10">칼로리&nbsp; &nbsp; &nbsp;(g)</td>
+              <td>
+                <InputCell />
+              </td>
+            </tr>
+            <tr>
+              <td className="pad-right-10">탄수화물 (g)</td>
+              <td>
+                <InputCell />
+              </td>
+            </tr>
+            <tr>
+              <td className="pad-right-10">단백질&nbsp; &nbsp; &nbsp;(g)</td>
+              <td>
+                <InputCell />
+              </td>
+            </tr>
+            <tr>
+              <td className="pad-right-10">지방&nbsp; &nbsp; &nbsp; &nbsp; (g)</td>
+              <td>
+                <InputCell />
+              </td>
+            </tr>
+            <tr>
+              <td className="pad-right-10">식이섬유 (g)</td>
+              <td>
+                <InputCell />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="submit_wrap">
+        <div id="submit_button_wrap">
+          <SubmitButton id="submit_item">저 장</SubmitButton>
+        </div>
+      </div>
+    </form>
+  );
+};
 
 // prettier-ignore
 const MainContainer = () => {
@@ -56,82 +130,13 @@ const MainContainer = () => {
     <div id="container" className="container">
       <div id="container_inner" className="container_inner table_container">
         <Today />
-        <form onSubmit={handleFormSubmit}>
-          <div className="main_wrap table_wrap">
-            <table className="simple_font">
-              <tbody>
-                <tr>
-                  <td className="pad-right-10">제품명</td>
-                  <td>
-                    <InputCell />
-                  </td>
-                </tr>
-                <tr>
-                  <td className="pad-right-10">업체명</td>
-                  <td >
-                    <InputCell />
-                  </td>
-                </tr>
-                <tr>
-                  <td className="pad-right-10">총량&nbsp; &nbsp; &nbsp; &nbsp;(g)</td>
-                  <td>
-                    <InputCell />
-                  </td>
-                </tr>
-                {/* 단위들 스팬에 넣고... 공백 맞추기!*/}
-                <tr>
-                  <td className="pad-right-10">
-                    섭취량&nbsp; &nbsp; &nbsp;(%)
-                  </td>
-                  <td>
-                    <InputCell />
-                  </td>
-                </tr>
-                <tr>
-                  <td className="pad-right-10">칼로리&nbsp; &nbsp; &nbsp;(g)</td>
-                  <td>
-                    <InputCell />
-                  </td>
-                </tr>
-                <tr>
-                  <td className="pad-right-10">탄수화물 (g)</td>
-                  <td>
-                    <InputCell />
-                  </td>
-                </tr>
-                <tr>
-                  <td className="pad-right-10">단백질&nbsp; &nbsp; &nbsp;(g)</td>
-                  <td>
-                    <InputCell />
-                  </td>
-                </tr>
-                <tr>
-                  <td className="pad-right-10">지방&nbsp; &nbsp; &nbsp; &nbsp; (g)</td>
-                  <td>
-                    <InputCell />
-                  </td>
-                </tr>
-                <tr>
-                  <td className="pad-right-10">식이섬유 (g)</td >
-                  <td>
-                    <InputCell />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div className="submit_wrap">
-            <div id="submit_button_wrap">
-              <SubmitButton id="submit_item">저 장</SubmitButton>
-            </div>
-          </div>
-        </form>
+        <InputForm />
       </div>
     </div>
   );
 };
 
-function MainPage() {
+function FoodFormPage() {
   return (
     <div id="wrap" className="wrap">
       <PageTitle>음식 입력하기</PageTitle>
@@ -141,4 +146,4 @@ function MainPage() {
   );
 }
 
-export default MainPage;
+export default FoodFormPage;
