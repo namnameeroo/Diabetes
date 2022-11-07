0151cefaadc21e9ce518aa5dada17b2094d9c8fe
HanaHww2 <hanahww2@gmail.com>
2022-11-07 19:45:39 +0900

    fix: 페이지네이션 수정 #35

--- back_end/src/main/java/com/diabetes/food/FoodRepository.java
+++ back_end/src/main/java/com/diabetes/food/FoodRepository.java
@@ -2,7 +2,9 @@ package com.diabetes.food;
 
 
 import com.diabetes.user.domain.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,7 +12,7 @@ import java.util.Optional;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
     List<Food> findAllByUser(User user);
-    List<Food> findAllByUserId(Long userId, PageRequest pageRequest);
+    Page<Food> findAllByUserId(Long userId, Pageable pageable);
 
     Optional<Food> findByIdAndUserId(Long foodId, Long userId);
 }
