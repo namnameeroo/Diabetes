a436a6f5a5c021770511770b3e465ad8f49ec595
namnameeroo <nami.skageec@gmail.com>
2022-11-22 18:18:08 +0900

    Feat:89:FoodForm POST API error fix, 결과보기 toggle 생성, modal생성중 resolve:#89

--- front-end/src/pages/foodForm.jsx
+++ front-end/src/pages/foodForm.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import {useState, useEffect} from "react";
+import { useState } from "react";
 import axios from "axios";
 import Utils from "utils";
 import Footer from "components/footer";
+// import CustomPopup from "components/modal";
+import ResultToggle from "components/toggle";
 import "styles/main.css";
 
 const PageTitle = (props) => {
@@ -13,20 +15,23 @@ const PageTitle = (props) => {
   );
 };
 
+const PopUpSuccess = (msg) => {
+  // 모달로 바꿔야 함
+  alert(msg);
+};
+
 const postData = async (inputs) => {
-  console.log("post data", inputs);
+  // console.log("post data", inputs);
+
   try {
     await axios
-      .post(Utils.baseUrl + `/api/v1/foods`, {
-        data: inputs,
-        withCredentials: true,
-      })
+      .post(Utils.baseUrl + `/api/v1/foods`, inputs, { withCredentials: true })
       .then((res) => {
         console.log(res);
-        console.log("form submit success");
+        if (res.status === 201) {
+          PopUpSuccess("결과가 저장되었습니다.");
+        }
       });
-
-    // console.log(res.data.result.content);
   } catch (e) {
     console.error(e);
   }
@@ -38,22 +43,71 @@ const Today = () => {
   let todayMonth = now.getMonth() + 1;
   let todayDay = now.getDate();
 
-  return <div className="right-align small-txt gray-txt">작성일 : {[todayYear, todayMonth, todayDay].join("-")}</div>;
+  return (
+    <div className="right-align small-txt gray-txt">
+      작성일 : {[todayYear, todayMonth, todayDay].join("-")}
+    </div>
+  );
 };
 
-const InputCell = ({label, onChangeInput, value}) => {
+const getGl = (inputs) => {
+
+  function betterFloat(num) {
+
+    // 간단 버전
+    return parseFloat(num.toFixed(10))
+  }
+
+  function multiply(num1, num2){
+    // 간단 버전
+    // Math.floor(parseFloat(num1)) toFixed()
+    return parseFloat(num1)*parseFloat(num2)
+  }
+
+  /*
+  * {entireWeight, calories, carbohydrate, protein, fat, fiber, intake, remains}
+  */
+  const proportion = parseFloat(inputs.intake/100) // 연산 확인 필요
+  const newInputs = {}
+  Object.entries(inputs).forEach(([key,value])=>
+  {(!!value || value===0 ) ? newInputs[key]=0 : newInputs[key]=value*proportion})
+  console.log(newInputs)
+
+
+  let result = 3.2+(0.393566429*(newInputs.carbohydrate-newInputs.fiber))-(0.205486363*newInputs.fat)-(0.006877061*newInputs.protein*newInputs.protein)-(0.012675566*newInputs.fiber*newInputs.fiber)
+
+  if (inputs.carbohydrate-inputs.fiber>=10) {
+    result += 3.2
+  }
+
+  let judges = result >= 20 ? "HIGH" : (result > 10 ? "MIDDLE" : "LOW")
+  return [result, judges]
+}
+
+const InputCell = ({ label, onChangeInput, value }) => {
   return (
     <div className="input_item" id="input_item_id">
-      <input className="input_text" name={label} onChange={onChangeInput} value={value}></input>
+      <input
+        className="input_text"
+        name={label}
+        onChange={onChangeInput}
+        value={value}
+      ></input>
     </div>
   );
 };
 const getNumOnly = (input) => {
-  return input.replaceAll(/[^0-9]*/g, "");
+  let nums = input.replaceAll(/[^0-9.]*/g, "");
+  if (nums.at(-1) === ".") {
+    
+    return parseInt(nums).toString() + ".";
+  } else {
+    nums = parseFloat(nums);
+  }
+  return nums;
 };
 
 const InputForm = () => {
-  // const [inputList, setInputList] = React.useState([""] * 10);
   const [inputs, setInputs] = React.useState({
     userId: 4,
     foodName: "",
@@ -67,28 +121,48 @@ const InputForm = () => {
     intake: 0,
     remains: 0,
     gl: 0,
-    result: "MIDDLE",
+    result: "",
   });
 
-  const {userId, foodName, provider, entireWeight, calories, carbohydrate, protein, fat, fiber, intake, remains, gl, result} = inputs;
+  /* eslint-disable-next-line*/
+  const { userId, foodName, provider, entireWeight, calories, carbohydrate, protein, fat, fiber, intake, remains, gl, result, } = inputs;
 
   const setMsg = (msg) => {
     console.log(msg);
   };
 
   const onChangeInput = (e) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     console.log(e.target, "target");
     const nextInput = {
       ...inputs,
       [name]: value,
     };
-
     setInputs(nextInput);
+    
+  };
+
+  const [toggleOpen, setToggleOpen] = useState(false);
+  // const [GL, setGL] = useState(0);
+  const onToggle = () => {
+    setToggleOpen(!toggleOpen);
+    const [newgl,newResult] = getGl(inputs)
+
+    const nextInput = {
+      ...inputs,
+      ["gl"]: newgl,
+      ["result"] : newResult,
+    };
+
+    console.log(newgl, newResult)
+    console.log(nextInput)
+    setInputs(nextInput);
+    console.log(inputs)
+
   };
 
   const onChangeInputForNum = (e) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     console.log(e.target, "target", value);
     if (isNaN(value)) {
       setMsg("숫자만 입력해주세요.");
@@ -98,6 +172,9 @@ const InputForm = () => {
       [name]: getNumOnly(value),
     };
     setInputs(nextInput);
+    setToggleOpen(false);
+    // 값이 변경되면 GL 결과를 다시 닫음
+
   };
 
   const onSubmit = (e) => {
@@ -106,10 +183,10 @@ const InputForm = () => {
 
   const handleSubmitClick = async () => {
     console.log("submit button clicked");
-    inputs.name = inputs.foodName; // 키 다른 거 수정
-
+    inputs.name = inputs.foodName;         // 키 달랐던 거 추가,
     await postData(inputs).then(() => console.log("posted"));
   };
+
 
   return (
     <>
@@ -120,17 +197,27 @@ const InputForm = () => {
               <tr>
                 <td className="pad-right-10">제품명</td>
                 <td>
-                  <InputCell label="foodName" onChangeInput={onChangeInput} value={inputs.foodName ? inputs.foodName : ""} />
+                  <InputCell
+                    label="foodName"
+                    onChangeInput={onChangeInput}
+                    value={inputs.foodName ? inputs.foodName : ""}
+                  />
                 </td>
               </tr>
               <tr>
                 <td className="pad-right-10">업체명</td>
                 <td>
-                  <InputCell label="provider" onChangeInput={onChangeInput} value={inputs.provider ? inputs.provider : ""} />
+                  <InputCell
+                    label="provider"
+                    onChangeInput={onChangeInput}
+                    value={inputs.provider ? inputs.provider : ""}
+                  />
                 </td>
               </tr>
               <tr>
-                <td className="pad-right-10">총량&nbsp; &nbsp; &nbsp; &nbsp;(g)</td>
+                <td className="pad-right-10">
+                  총량&nbsp; &nbsp; &nbsp; &nbsp;(g)
+                </td>
                 <td>
                   <InputCell
                     label="entireWeight"
@@ -144,7 +231,12 @@ const InputForm = () => {
               <tr>
                 <td className="pad-right-10">섭취량&nbsp; &nbsp; &nbsp;(%)</td>
                 <td>
-                  <InputCell label="intake" types="number" onChangeInput={onChangeInputForNum} value={inputs.intake ? inputs.intake : 0} />
+                  <InputCell
+                    label="intake"
+                    types="number"
+                    onChangeInput={onChangeInputForNum}
+                    value={inputs.intake ? inputs.intake : 0}
+                  />
                 </td>
               </tr>
               <tr>
@@ -181,42 +273,68 @@ const InputForm = () => {
                 </td>
               </tr>
               <tr>
-                <td className="pad-right-10">지방&nbsp; &nbsp; &nbsp; &nbsp; (g)</td>
+                <td className="pad-right-10">
+                  지방&nbsp; &nbsp; &nbsp; &nbsp; (g)
+                </td>
                 <td>
-                  <InputCell label="fat" types="number" onChangeInput={onChangeInputForNum} value={inputs.fat ? inputs.fat : 0} />
+                  <InputCell
+                    label="fat"
+                    types="number"
+                    onChangeInput={onChangeInputForNum}
+                    value={inputs.fat ? inputs.fat : 0}
+                  />
                 </td>
               </tr>
               <tr>
                 <td className="pad-right-10">식이섬유 (g)</td>
                 <td>
-                  <InputCell label="fiber" types="number" onChangeInput={onChangeInputForNum} value={inputs.fiber ? inputs.fiber : 0} />
+                  <InputCell
+                    label="fiber"
+                    types="number"
+                    onChangeInput={onChangeInputForNum}
+                    value={inputs.fiber ? inputs.fiber : 0}
+                  />
                 </td>
               </tr>
             </tbody>
           </table>
         </div>
       </form>
-      <SubmitButton id="submit_item" handleSubmitClick={handleSubmitClick}>
+      <ResultToggle onToggle={onToggle} open={toggleOpen} result={inputs.result + " " + inputs.gl}>결 과 보 기</ResultToggle>
+      
+      <SubmitButton handleSubmitClick={handleSubmitClick}>
         저 장
       </SubmitButton>
+
+      {/*  모달... <button onClick={(e) => setVisibility(!visibility)}>Toggle Popup</button>
+        <CustomPopup onClose={popupCloseHandler} show={visibility} title="Hello Jeetendra">
+          <h1>Hello This is Popup Content Area</h1>
+          <h2>This is my lorem ipsum text here!</h2>
+        </CustomPopup> */}
     </>
   );
 };
 
 const SubmitButton = (props) => {
   return (
-    <div className="submit_wrap">
-      <div id="submit_button_wrap">
-        <button types="submit" className="btn_submit" id={props.id} onClick={props.handleSubmitClick}>
+    <div className="btn_wrap">
+
+        <button
+          type="submit"
+          className="btn_submit"
+          id={props.id}
+          onClick={props.handleSubmitClick}
+        >
           <span className="btn_text">{props.children}</span>
         </button>
-      </div>
+
     </div>
   );
 };
 
+
 // prettier-ignore
-const MainContainer = () => {
+const MainForm = () => {
   return (
     <div id="container" className="container">
       <div id="container_inner" className="container_inner table_container">
@@ -228,11 +346,21 @@ const MainContainer = () => {
 };
 
 function FoodFormPage() {
+  const [visibility, setVisibility] = useState(false);
+  const popupCloseHandler = (e) => {
+    setVisibility(e);
+  };
+
   return (
     <div id="wrap" className="wrap">
       <PageTitle>음식 입력하기</PageTitle>
-      <MainContainer />
+      <MainForm />
       <Footer />
+
+      {/* <CustomPopup onClose={popupCloseHandler} show={visibility} title="Hello Jeetendra"> */}
+      {/* <h1>Hello This is Popup Content Area</h1>
+        <h2>This is my lorem ipsum text here!</h2>
+      </CustomPopup> */}
     </div>
   );
 }
