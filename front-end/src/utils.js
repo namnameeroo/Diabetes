af0f39ffa75ea624f7972dd093fc8ca03837ccab
namnameeroo <nami.skageec@gmail.com>
2022-12-06 19:34:47 +0900

    Feat:109:updateBaseUrl resolves: #109

--- front-end/src/utils.js
+++ front-end/src/utils.js
@@ -1,7 +1,7 @@
 const Utils = {
-  // baseUrl: "http://ec2-15-165-155-229.ap-northeast-2.compute.amazonaws.com:8080",
   // baseUrl: "http://localhost:8080",
-  baseUrl: "http://ec2-13-124-87-106.ap-northeast-2.compute.amazonaws.com:8080",
+  // baseUrl: "http://ec2-13-124-87-106.ap-northeast-2.compute.amazonaws.com:8080",
+  baseUrl: "https://gl-diabetes.shop",
 };
 
 export default Utils;
