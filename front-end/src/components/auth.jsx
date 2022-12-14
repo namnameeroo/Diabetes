96b929f3e5c53e3ae46bb1def140a2b625c375e2
namnameeroo <nami.skageec@gmail.com>
2022-12-14 11:52:28 +0900

    Test:123:asyncTestToFixRedirect #123

--- front-end/src/components/auth.jsx
+++ front-end/src/components/auth.jsx
@@ -11,14 +11,14 @@ import Utils from "utils";
  * 유저 정보 가져와서 로그인 처리
  * App.js 로부터 전달받은 상태관리 함수
  */
-const Auth = () => {
+const Auth = async () => {
   /* eslint-disable-next-line*/
   const [errorMsg, setErrorMsg] = useState("Auth 실패");
   const [isAdmin, setIsAdmin] = useState(false);
   const [isLogin, setIsLogin] = useState(false);
 
   /* eslint-disable-next-line*/
-  useEffect(() => {
+  await useEffect(() => {
     const getUser = async () => {
       console.log("getUser inner");
       try {
@@ -40,25 +40,24 @@ const Auth = () => {
 
     getUser();
   }, []);
-  useEffect(() => {
-    return (
-      <>
-        {
-          (console.log("🚀 ~ file: auth.jsx:47 ~ return ~ isAdmin", isAdmin),
-          console.log("🚀 ~ file: auth.jsx:48 ~ return ~ isLogin", isLogin))
-        }
 
-        {isLogin ? (
-          isAdmin ? (
-            <Navigate to="/adminUserList" replace={true} />
-          ) : (
-            <Navigate to="/mylist" replace={true} />
-          )
+  return (
+    <>
+      {
+        (console.log("🚀 ~ file: auth.jsx:47 ~ return ~ isAdmin", isAdmin),
+        console.log("🚀 ~ file: auth.jsx:48 ~ return ~ isLogin", isLogin))
+      }
+
+      {isLogin ? (
+        isAdmin ? (
+          <Navigate to="/adminUserList" replace={true} />
         ) : (
-          <Navigate to="/foodForm" replace={true} />
-        )}
-      </>
-    );
-  }, []);
+          <Navigate to="/mylist" replace={true} />
+        )
+      ) : (
+        <Navigate to="/foodForm" replace={true} />
+      )}
+    </>
+  );
 };
 export default Auth;
