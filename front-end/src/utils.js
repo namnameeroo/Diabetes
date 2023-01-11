0a88ad62e53c0fb49bebd630becc40955220e9b3
namnameeroo <nami.skageec@gmail.com>
2023-01-11 13:17:55 +0900

    Fix:170:Chore:fix in app, foodform, utils chores

--- front-end/src/utils.js
+++ front-end/src/utils.js
@@ -1,5 +1,4 @@
 const Utils = {
-  // baseUrl: "http://localhost:3000"
   // baseUrl: "http://ec2-13-124-87-106.ap-northeast-2.compute.amazonaws.com:8080",
   // baseUrl: "http://localhost:8080"
   baseUrl: "https://gl-diabetes.shop"
