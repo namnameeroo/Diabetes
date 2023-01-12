11162a120efbb4b251d1fa9601e1ecc813b392b6
namnameeroo <nami.skageec@gmail.com>
2023-01-12 13:24:11 +0900

    Feat:175:리다이렉트 컴포넌트에서 Navigate by role,auth상태 resolves: #175

--- front-end/src/pages/redirect.jsx
+++ front-end/src/pages/redirect.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+
 // import Auth from "components/auth";
 
 import { useContext } from "react";
@@ -14,6 +16,20 @@ const RedirectPage = () => {
         유저 이름 : {user ? user.info.email : null}
       </div>
       <div style={{ height: "50px" }}>유저 이름 : {"redirect"}</div>
+
+      {/* 로그인 안 한 상태면, 로그인 페이지로
+          한 상태면 role에 따라 분기처리
+      */}
+
+      {user.info.auth ? (
+        user.info.role == "USER" ? (
+          <Navigate to="/mylist" />
+        ) : (
+          <Navigate to="/adminUserList" />
+        )
+      ) : (
+        <Navigate to="/login" />
+      )}
     </>
   );
 };
