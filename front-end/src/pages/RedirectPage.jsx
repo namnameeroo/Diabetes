e8b706d96150977af940dc117c8f7e73f505afe5
namnameeroo <nami.skageec@gmail.com>
2023-03-27 18:02:11 +0900

    Refactor:207:remove consoles, code cleaning

--- front-end/src/pages/RedirectPage.jsx
+++ front-end/src/pages/RedirectPage.jsx
@@ -1,40 +1,9 @@
 import React from "react";
-// import { getCurrentUser } from "api/login";
-// import Auth from "components/auth";
-// import { useEffect } from "react";
-
-// import { useState } from "react";
-// import { UserContext } from "components/userContext";
-
 const RedirectPage = () => {
-  // const { user } = useContext(UserContext); // !important
-  // if (currentUser != user) {
-  //   setCurrentUser(user);
-  // }
-
   return (
     <>
-      {/* <Auth /> */}
-      <div style={{ height: "50px" }}>
-        []
-        {/* 유저 이름 : {currentUser ? currentUser.email : null} */}
-      </div>
+      <div style={{ height: "50px" }}></div>
       <div style={{ height: "50px" }}>유저 이름 : {"redirect"}</div>
-
-      {/* 로그인 안 한 상태면, 로그인 페이지로
-          한 상태면 role에 따라 분기처리
-      */}
-
-      {/*       
-      {user.auth ? (
-        user.role == "USER" ? (
-          <Navigate to="/mylist" />
-        ) : (
-          <Navigate to="/adminUserList" />
-        )
-      ) : (
-        <Navigate to="/login" />
-      )} */}
     </>
   );
 };
