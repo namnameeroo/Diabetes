edd97e1101feabf8a992d3af02f250700eaebbdf
namnameeroo <nami.skageec@gmail.com>
2023-03-31 17:17:39 +0900

    Refactor:Fix:207:redirect page to move other page

--- front-end/src/pages/RedirectPage.jsx
+++ front-end/src/pages/RedirectPage.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+// import { Navigate } from "react-router-dom";
+
 const RedirectPage = () => {
   return (
     <>
-      <div style={{ height: "50px" }}></div>
-      <div style={{ height: "50px" }}>유저 이름 : {"redirect"}</div>
+      {/* <Navigate to="/mylist" /> */}
+      <>
+        <div style={{ height: "50px" }}>Redirect</div>
+        <div style={{ height: "500px" }}>
+          <a href="/login">
+            login
+            <br />
+          </a>
+          <a href="/foodForm">
+            입력 폼
+            <br />
+          </a>
+          <a href="/mylist">
+            입력 현황
+            <br />
+          </a>
+          <a href="/userlist">
+            유저 현황
+            <br />
+          </a>
+          <a href="/">
+            관리 계정 신청
+            <br />
+          </a>
+        </div>
+      </>
     </>
   );
 };
